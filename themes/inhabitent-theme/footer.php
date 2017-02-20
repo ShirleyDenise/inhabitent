36cef7dad3ea789c170a6ca35c45e597f6b09dc5
Shirley <ma_shirley@msn.com>
2017-02-19 21:31:06 -0800

    sidebar 80 footer 60.

--- themes/inhabitent-theme/footer.php
+++ themes/inhabitent-theme/footer.php
@@ -10,6 +10,38 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="bloc-contact">
+					<h3>Contact Info</h3>
+					<p>
+						<i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:info@inhabitent.com">info@inhabitent.com</a>
+					</p>
+					<p>
+						<i class="fa fa-phone" aria-hidden="true"></i><a href="tel:7784567891">778-456-7891</a>
+					</p>
+					<div class="social-icon">
+						<i class="fa fa-facebook-square" aria-hidden="true"></i><i class="fa fa-twitter-square" aria-hidden="true"></i><i class="fa fa-google-plus-square" aria-hidden="true"></i>
+					</div>
+					
+				</div>
+				<div class="bloc-hours">
+					<h3>Business hours</h3>
+					<p>
+						<span class="day">Monday-Friday</span>: 9am to 5pm
+					</p>
+					<p>
+						<span class="day">Saturday</span>: 10am to 2pm
+					</p>
+					<p>
+						<span class="day">Sunday</span>: Closed
+					</p>
+				</div>
+				<div class="footer-logo">
+					<a href="home" rel="home">
+						<img src="/images/inhabitent-logo-text.svg" alt="text-logo"/>
+						<p><span class="text-inha">Inhabitent</span></p>
+						<p><span class="text-camp">Camping Supply</span> Co.</p>
+					</a>
+				</div>
 				<div class="copyright">Copyright © 2016 Inhabitent</div>
 				<div class="site-info">
 					<!--<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Copyright © 2016 Inhabitent' ), 'WordPress' ); ?></a>-->
