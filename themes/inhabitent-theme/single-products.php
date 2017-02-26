771c776e80acf79e58757a1ad7e4d4cb4e54352f
Shirley <ma_shirley@msn.com>
2017-02-26 12:59:37 -0800

    single-product.

--- themes/inhabitent-theme/single-products.php
+++ themes/inhabitent-theme/single-products.php
@@ -1,33 +1,37 @@
 <?php
 /**
- * Template part for displaying single posts.
+ * The template for displaying single products.
  *
  * @package RED_Starter_Theme
  */
-
 get_header(); ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-		<div class="entry-meta">
-			<?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?> / <?php red_starter_posted_by(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
+		      <article class="single_product_wrapper" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		      	<header class="product-image">
+		      		<?php if ( has_post_thumbnail() ) : ?>
+		      			<?php the_post_thumbnail( 'large' ); ?>
+		      		<?php endif; ?>
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+		      	</header><!-- .entry-header -->
+
+		      	<div class="product-entry">
+							<?php the_title( '<h1 class="product-title">', '</h1>' ); ?>
+							<p class="single-price"><?php echo CFS()->get('product_price'); ?></p>
+		      		<div class="single-content"><?php the_content(); ?></div>
+		      	</div><!-- .entry-content -->
+		      </article><!-- #post-## -->
+
+
+
+
+		<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php get_footer(); ?>
